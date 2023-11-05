f761cf248ab8b2677c6068575b1b61bdfa333544
Ziqi Liu <ziqi.liu1207@gmail.com>
2023-11-05 00:04:22 -0700

    finish the structure of sidebar, finish sign in firestore, working on the post content

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -3,11 +3,34 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
     callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
             // User successfully signed in.
             // Return type determines whether we continue the redirect automatically
             // or whether we leave that to developer to handle.
-            return true;
+            //------------------------------------------------------------------------------------------
+            // The code below is modified from default snippet provided by the FB documentation.
+            //
+            // If the user is a "brand new" user, then create a new "user" in your own database.
+            // Assign this user with the name and email provided.
+            // Before this works, you must enable "Firestore" from the firebase console.
+            // The Firestore rules must allow the user to write.
+            //------------------------------------------------------------------------------------------
+            var user = authResult.user;                            // get the user object from the Firebase authentication database
+            if (authResult.additionalUserInfo.isNewUser) {         //if new user
+                db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
+                    name: user.displayName,                    //"users" collection
+                    email: user.email,                         //with authenticated user's ID (user.uid)
+                    postalcode: "V4N 4Y2"                         //optional default profile info
+                }).then(function () {
+                    console.log("New user added to firestore");
+                    window.location.assign("main.html");       //re-direct to main.html after signup
+                }).catch(function (error) {
+                    console.log("Error adding new user: " + error);
+                });
+            } else {
+                return true;
+            }
+            return false;
         },
         uiShown: function() {
             // The widget is rendered.
@@ -17,15 +40,15 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'main.html',
+    signInSuccessUrl: "main.html",
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
-        //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
     tosUrl: '<your-tos-url>',
--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,24 @@ openSideBarE1.addEventListener("click", ()=>{
 closeSideBarE1.addEventListener("click", ()=>{
     sideBarE1.style.display = "none"
 })
+
+function insertNameFromFirestore() {
+    // Check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                var userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); // jQuery
+                document.getElementById("username-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
+    })
+}
+
+insertNameFromFirestore();
