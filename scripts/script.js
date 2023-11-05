b989f8a84758a91f86430472943e105f43e625fa
Ziqi Liu <ziqi.liu1207@gmail.com>
2023-11-04 19:04:11 -0700

    make the sidebar and refine the main page

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
-function sayHello() {
-    
-}
-//sayHello();
+let openSideBarE1 = document.getElementById("open-sideBar")
+const sideBarE1 = document.getElementById("sideBar")
+const closeSideBarE1 = document.getElementById("close-sidebar")
+
+openSideBarE1.addEventListener("click", ()=>{
+    sideBarE1.style.display = "block"
+})
+
+closeSideBarE1.addEventListener("click", ()=>{
+    sideBarE1.style.display = "none"
+})
