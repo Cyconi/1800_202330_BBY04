4c109a47fef740cb78368583bdcb55289159d498
Radman Mohammadi <radmanon01@gmail.com>
2023-11-16 11:38:36 -0800

    the chat.js updated for generating different color for user's message

--- scripts/chat.js
+++ scripts/chat.js
@@ -40,3 +40,37 @@ function displayMessage(messageData) {
 
     messageList.scrollTop = messageList.scrollHeight
 }
+
+
+
+
+
+const userColors = {};
+
+// Function to generate a random color for each messages in chat
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Update your displayMessage function
+function displayMessage(messageData) {
+    const messageList = document.querySelector('#messageList');
+    const messageElement = document.createElement('li');
+
+    // Check if the user already has a color, otherwise assign a new random color
+    const userColor = userColors[messageData.userName] || getRandomColor();
+    userColors[messageData.userName] = userColor;
+
+    // Apply the user's color to the message text
+    messageElement.style.color = userColor;
+    messageElement.textContent = `${messageData.userName}: ${messageData.message}`;
+
+    messageList.appendChild(messageElement);
+
+    messageList.scrollTop = messageList.scrollHeight;
+}
