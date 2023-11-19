57da1a1f32ae9b4848ddaa4979d3130d00f8568d
Ziqi Liu <ziqi.liu1207@gmail.com>
2023-11-18 17:28:15 -0800

    finish the javascript for feedback section

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,1 +1,39 @@
+function displayFeedback () {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            let feedbackTemplate = document.querySelector('#feedback-template')
+            let userID = user.uid
+            db.collection('users').doc(userID).get().then(userDoc => {
+                let userLocation = userDoc.data().location
 
+                db.collection('feedbacks').get().then(allFeedback => {
+                    allFeedback.forEach(doc => {
+                        let posterLocation = doc.data().posterLocation
+
+                        if(posterLocation === userLocation) {
+                            let newFeedback = feedbackTemplate.content.cloneNode(true)
+
+                            newFeedback.querySelector('.feedback-link').href = 'eachFeedback.html?docId=' + doc.id
+                            newFeedback.querySelector('.feedback-title').innerText = doc.data().title
+                            newFeedback.querySelector('.feedback-detail').innerText = doc.data().text
+                            newFeedback.querySelector('.feedback-likes-number').innerText = doc.data().likesNumber
+                            newFeedback.querySelector('.feedback-comments-number').innerText = doc.data().commentsNumber
+                            newFeedback.querySelector('.feedback-photo-container').id = `${doc.id}`
+
+                            if (doc.data().photoURL) {
+                                let imgElement = newFeedback.querySelector('.feedback-photo')
+                                imgElement.src = doc.data().photoURL
+                                imgElement.style.display = 'block';
+                            }
+                            document.querySelector('#feedbacks-goes-here').appendChild(newFeedback)
+                            if (!doc.data().photoURL) {
+                                document.getElementById(`${doc.id}`).style.display = 'none'
+                            }
+                        }
+                    })
+                })
+            })
+        }
+    })
+}
+displayFeedback()
---
+++ scripts/feedbackpost.js
@@ -0,0 +1,50 @@
+document.querySelector('#postButton').addEventListener('click', function(){
+    let feedbackTitle = document.querySelector('#feedback-title').value
+    let feedbackText = document.querySelector('#user-story').value
+    let feedbackPhoto = document.querySelector('#photoUpload').files[0]
+
+    if (feedbackPhoto) {
+        let storageRef = firebase.storage().ref('photos-feedback/' + feedbackPhoto.name)
+        storageRef.put(feedbackPhoto).then(function(snapshot) {
+            snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                createFeedbackWithImage(feedbackTitle, feedbackText, downloadURL)
+            })
+        })
+    } else {
+        createFeedbackWithImage(feedbackTitle, feedbackText, null)
+    }
+})
+
+function createFeedbackWithImage(title, text, photoURL){
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            let userID = user.uid
+
+            db.collection('users').doc(userID).get().then(userDoc => {
+
+                let feedback = {
+                    posterName: userDoc.data().name,
+                    posterID: userID,
+                    posterLocation: userDoc.data().location,
+                    title: title,
+                    text: text,
+                    likesNumber: 0,
+                    commentsNumber: 0,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                }
+                if (photoURL) {
+                    feedback.photoURL =  photoURL
+                }
+
+                db.collection('feedbacks').add(feedback).then(function(){
+                    console.log("feedback sent")
+                    window.location.assign("feedback.html")
+                })
+
+
+
+            })
+        }
+    })
+
+}
